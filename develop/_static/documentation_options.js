551a1f589c33d65866f50aa2fda5e06d63c7d2a1
i-am-sijia <i-am-sijia@users.noreply.github.com>
2025-06-01 19:07:08 +0000

    deploy: 4063cd6b5213327697fe0d4e85961ebbab61b5a3

--- develop/_static/documentation_options.js
+++ develop/_static/documentation_options.js
@@ -1,14 +1,13 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.1.1.dev68+g7ead422d',
-    LANGUAGE: 'None',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '1.1.4.dev1490+g4063cd6b',
+    LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
     LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: true,
+    NAVIGATION_WITH_KEYS: false,
     SHOW_SEARCH_SUMMARY: true,
     ENABLE_SEARCH_SHORTCUTS: true,
 };
